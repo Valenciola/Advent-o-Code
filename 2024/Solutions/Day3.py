709c8e1654d58c821dbfddeeb46d5dc8dbeb0052
Valenciola <valeriez3012@gmail.com>
2024-12-03 19:37:58 -0500

    Part 2 done but tomorrow better be better

--- 2024/Solutions/Day3.py
+++ 2024/Solutions/Day3.py
@@ -10,23 +10,47 @@ for x in range (0, 6):
 muls = re.findall(r"mul\(\d{1,3},\d{1,3}\)", memory)
 multinums = []
 total = 0
+flag = 1
+
+matchin = re.finditer(r"mul\(\d{1,3},\d{1,3}\)", memory, re.IGNORECASE)
+matchins = []
+for match in matchin:
+    matchins.append(match.start())
+
+do = re.finditer(r"do\(\)", memory, re.IGNORECASE)
+dos = []
+for match in do:
+    dos.append(match.start())
+
+dont = re.finditer(r"don't\(\)", memory, re.IGNORECASE)
+donts = []
+for match in dont:
+    donts.append(match.start())
 
 
+# Final split and calculations
 for x in range (0, len(muls)):
     muls[x] = muls[x].replace("mul(", "")
     muls[x] = muls[x].replace(")", "")
     multinums.append(muls[x].split(","))
 
-for x in multinums:
-    if (not(x[0].isdigit() and x[1].isdigit())):
-        multinums.remove(x)
-
-for x in range (0, len(multinums)):
-    multi = int(multinums[x][0]) * int(multinums[x][1])
-    total = total + multi
+for x in range (0, matchins[len(matchins) - 1] + 1):
+    #print(x)
+    if (x in dos):
+        flag = 1
+        #print("a do")
+    elif (x in donts):
+        flag = 0
+        #print("a don't")
+    
+    if ((x in matchins) and (flag)):
+        #print("A match!")
+        chap = matchins.index(x)
+        multi = int(multinums[chap][0]) * int(multinums[chap][1])
+        total = total + multi
+    else:
+        continue
 
 print("Total multiplications gets us to " + str(total))
-
-# 189331402 is too low
 
 memoryFile.close()
