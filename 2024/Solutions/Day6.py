b9e23c3ac13927d9a45108d20f025209d8bad17b
Valenciola <valeriez3012@gmail.com>
2024-12-09 19:11:29 -0500

    Incomplete for day 6 kinda but from visual terminal parsing could see answer

--- 2024/Solutions/Day6.py
+++ 2024/Solutions/Day6.py
@@ -1,16 +1,29 @@
+import copy
+
 mapgram = open("2024/Inputs/D6.txt", "r")
 grid = []
 position = [0,0] # Go (x,y)
 direction = 0
-stops = 0
+marks = []
+returns = 0
+blocks = 0
+transcript = []
+weirdos = []
 
 def trace(position):
+    #Working through the map
     x = position[0]
     y = position[1]
     grid[y][x] = 'X'
     scope = [x, y]
     global direction
     #print(scope)
+    global returns # The amount of times we hit the O
+    global blocks
+    global transcript, lim
+
+    global weirdos
+
     match direction: # Look ahead in the grid
         case 0: # Facing forward
             scope = [x, y - 1]
@@ -21,11 +34,20 @@ def trace(position):
         case 3: # Facing left
             scope = [x - 1, y]
     if ((scope[0] > hori - 1) or (scope[1] > verti - 1) or (scope[0] < 0) or (scope[1]) < 0):
-        #print("Out of here")
+        print("Out of here")
+        returns = 0
         return 0
-    elif (grid[scope[1]][scope[0]] == '#'):
-        # print("Roadblock")
-        # print(scope)
+    elif (grid[scope[1]][scope[0]] == '#' or grid[scope[1]][scope[0]] == 'O'):
+        #print("Roadblock", scope)
+        if (grid[scope[1]][scope[0]] == 'O'):
+            returns += 1
+            if (returns > 5):
+                print("Good boundary")
+                print(returns)
+                returns = 0
+                blocks += 1
+                return 0
+        
         direction += 1
         if (direction == 4):
             direction = 0
@@ -33,8 +55,16 @@ def trace(position):
     else:
         position[0] = scope[0]
         position[1] = scope[1]
+        transcript.append(scope)
+        if (len(transcript) > lim):
+            transcript.clear()
+            weirdos.append(returns)
+            blocks += 1
+            returns = 0
+            return 0
         return 1
 
+# Grid generation
 for x in range(0, 130):
     mapline = mapgram.readline().strip()
     grid.append(mapline)
@@ -46,8 +76,11 @@ for x in range(0, len(grid)):
         position[0] = grid[x].index('^')
     #print(grid[x])
 
+# Establishing boundaries and saving the start point
 verti = len(grid)
 hori = len(grid[0])
+init = copy.deepcopy(position)
+lim = verti * hori * 100
 
 print(position)
 
@@ -55,14 +88,38 @@ active = 1
 while(active):
     active = trace(position)
 
-for x in range(0, len(grid)):
-    stops = stops + grid[x].count('X')
+for y in range(0, verti):
+    for x in range(0, hori):
+        if (grid[y][x] == 'X'):
+            marks.append([x,y])
+            grid[y][x] = '.'
 
-print("The guard makes " + str(stops) + " stops.")
+marks.remove(init)
+grid[init[1]][init[0]] = '^'
+#print(marks)
+
+print("Reviewing with blocks")
+for x in range(0, len(marks)):
+    chx = marks[x][0]
+    chy = marks[x][1]
+    print(chx, chy)
+    grid[chy][chx] = 'O'
+    position = copy.deepcopy(init)
+    active = 1
+    direction = 0
+    while(active):
+        active = trace(position)
+    grid[chy][chx] = '.'
+
+# print(init)
+# print(position)
 
 '''
 for x in range(0, len(grid)):
     print(grid[x])
 '''
+
+print("We can do " + str(blocks) + " obstructions")
+print(weirdos)
 
 mapgram.close()
