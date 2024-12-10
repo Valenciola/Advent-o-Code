876009f9d0731c2b55ded6ba770b8417a5e7e2df
Valenciola <valeriez3012@gmail.com>
2024-12-10 00:49:22 -0500

    Part 2 required some more brain flexing

--- 2024/Solutions/Day9.py
+++ 2024/Solutions/Day9.py
@@ -1,3 +1,5 @@
+import copy
+
 grab = open("2024/Inputs/D9.txt", "r")
 line = grab.readline()
 #print(line)
@@ -24,19 +26,28 @@ for x in range(0, len(blocks)):
         mem.append('.')
 maxid = x
 
+def findconti(space):
+    global mem
+    opening = 0
+    for x in range(0, len(mem)):
+        #print(x)
+        if (str(mem[x]).isdigit()):
+            opening = 0
+        elif (mem[x] == '.'):
+            opening += 1
+            if (opening == space):
+                return (x - opening + 1)
+    return 0
+
+for x in range(maxid, -1, -1):
+    avail = findconti(mem.count(x))
+    if (avail and not (avail > mem.index(x))):
+        for z in range(0, mem.count(x)):
+            #print(avail + z)
+            mem[mem.index(x, (avail + z))] = '.'
+            mem[avail + z] = x
 #print(mem)
-
-for x in mem:
-    #print(x)
-    if (str(x).isdigit()):
-        continue
-    else:
-        while (mem[len(mem) - 1] == '.'):
-            mem.pop(len(mem) - 1)
-        mem[mem.index(x)] = mem[len(mem) - 1]
-        mem.pop(len(mem) - 1)
-    #print(mem)
-
+    
 for x in range(0, len(mem)):
     #print(x, mem[x])
     if (mem[x] == '.'):
