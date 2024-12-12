fb1965e5f7f4ee7bf105b1bcac09b91089edf23f
Valenciola <valeriez3012@gmail.com>
2024-12-11 23:21:12 -0500

    Programmer Kid saves the day once again

--- 2024/Solutions/Day11.py
+++ 2024/Solutions/Day11.py
@@ -1,6 +1,70 @@
 mapped = open("2024/Inputs/D11.txt", "r")
 row = mapped.readline()
 stones = row.split()
+for f in stones:
+    stones[stones.index(f)] = int(f)
+
+def helpmeh(stones, blinks):
+    stoneob = {} # I've literally never touched dictionaries I miss you Javascript ;-;
+    cache = {}
+
+    for x in stones:
+        if(not x in stoneob):
+            stoneob[x] = 0
+        stoneob[x] += 1
+
+    for i in range(0, blinks):
+        newstoneob = {}
+        for s in list(stoneob.keys()):
+            stoco = stoneob[s]
+
+            if (not s in cache): # Cache stuff which is fun to explore
+                if (s == 0):
+                    cache[s] = 1
+                elif(len(str(s)) % 2 == 0):
+                    broke = int(len(str(s)) / 2)
+                    cache[s] = [int(str(s)[0:broke]), int(str(s)[broke:len(str(s))])]
+                else:
+                    cache[s] = s * 2024
+            
+            if (len(str(s)) % 2 == 0):
+                if (not cache[s][0] in newstoneob):
+                    newstoneob[cache[s][0]] = 0
+                if (not cache[s][1] in newstoneob):
+                    newstoneob[cache[s][1]] = 0
+                
+                newstoneob[cache[s][0]] += stoco
+                newstoneob[cache[s][1]] += stoco
+            else:
+                if (not cache[s] in newstoneob):
+                    newstoneob[cache[s]] = 0
+                
+                newstoneob[cache[s]] += stoco
+            
+            for a in newstoneob:
+                if newstoneob[a] == 0:
+                    del newstoneob[a]
+        
+        stoneob = newstoneob
+    
+    stonecount = 0
+    for y in stoneob:
+        stonecount += stoneob[y]
+
+    return stonecount
+
+print(stones)
+sol = helpmeh(stones, 75)
+print(sol)
+
+mapped.close()
+
+'''
+# The part 1 solution that just wasn't meant to be reused (sobs) rest in comments
+
+mapped = open("2024/Inputs/D11.txt", "r")
+row = mapped.readline()
+stones = row.split()
 
 for f in stones:
     stones[stones.index(f)] = int(f)
@@ -34,3 +98,4 @@ for a in range(0, 25):
 print("We got " + str(len(stones)) + " stones!")
 
 mapped.close()
+'''
