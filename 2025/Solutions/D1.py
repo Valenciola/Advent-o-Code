a0356968a9f7b4f4dd4ede3693bc9a7fe758f742
Valenciola <valeriez3012@gmail.com>
2025-12-10 21:24:29 -0500

    Part 2 Day 1 complete

--- 2025/Solutions/D1.py
+++ 2025/Solutions/D1.py
@@ -1,10 +1,50 @@
+# PART 2
+ticks = open("2025/Inputs/D1.txt")
+position = 50
+stops = []
+passes = 0
+
+for tick in ticks.readlines():
+    direction = tick[0]
+    click = int(tick[1:])
+
+    passes = passes + int(click / 100)
+    click = click % 100
+
+    if direction == 'L':
+        stop = position - click
+        end = stop
+        if stop < 0:
+            stop = stop + 100
+    else:
+        stop = position + click
+        end = stop
+        if stop > 99:
+            stop = stop - 100
+    
+    print(tick.strip(), position, end)
+
+    # Handle passes
+    if position == 0 or end == 0:
+        pass
+    else:
+        if (position > 0 and end < 0) or (position > 0 and end > 100):
+            # print("Passed 0")
+            passes = passes + 1
+     
+    stops.append(stop)
+
+    position = stop
+
+print(f"0s in the combo: {stops.count(0) + passes}")
+
+'''
+PART 1
 ticks = open("2025/Inputs/D1.txt")
 position = 50
 stops = []
 
-'''
 Notes: 100 ticks is a full rotation, so the number would remain the same. Exclude full revolutions first?
-'''
 
 for tick in ticks.readlines():
     direction = tick[0]
@@ -26,3 +66,4 @@ for tick in ticks.readlines():
     position = stop
 
 print(f"0s in the combo: {stops.count(0)}")
+'''
